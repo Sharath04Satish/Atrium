938f544b523992f57862c0c34764d0b31cbcd054
Sharath S <30643120+Sharath04Satish@users.noreply.github.com>
2020-05-17 13:25:15 +0530

    Update routes.py

--- contents/routes.py
+++ contents/routes.py
@@ -69,13 +69,9 @@ def csvUpload():
 
         files = glob('D:\\Atrium\\User Uploads\\*') 
         latest_file = max(files, key=os.path.getctime)
-        print(latest_file)
 
         df = pd.read_csv(latest_file, header=None)
         X_test_upload = df.values[:, :-1]
-        y_test_upload = df.values[:, -1].astype(int)
-
-        y_test_upload = to_categorical(y_test_upload)
 
         prediction = model.predict(X_test_upload)
 
